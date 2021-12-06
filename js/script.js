e2e7d2da1473babb583d6f3550fced4265e21e78
Otabek <79497492+otabek-oktamov@users.noreply.github.com>
2021-12-06 09:06:38 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,27 +29,9 @@ const account1 = {
   locale: 'pt-PT', // de-DE
 };
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
 
-  movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
-  ],
-  currency: 'USD',
-  locale: 'en-US',
-};
 
-const accounts = [account1, account2];
+const accounts = [account1, ];
 
 /////////////////////////////////////////////////
 // Elements
